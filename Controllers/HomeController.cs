5d366cfd3edf195ecfee0d400686517d149b0dee
Momen safi <Safi.moumen90@gmail.com>
2023-12-22 20:48:39 +0300

    done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using MVC3.Data;
 using MVC3.Models;
 using System.Diagnostics;
@@ -8,10 +9,12 @@ namespace MVC3.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -23,8 +26,18 @@ namespace MVC3.Controllers
         {
             return View();
         }
-        
 
+        public ActionResult Logout()
+        {
+            HttpContext.SignOutAsync();
+
+            // Disable caching
+            Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+            Response.Headers["Pragma"] = "no-cache";
+            Response.Headers["Expires"] = "0";
+
+            return RedirectToAction("LogIn", "Manager");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
